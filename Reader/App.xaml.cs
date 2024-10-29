cc68bdfc07b064ba88049aae653b7f411fc59404
6a9HEAST <lyovalite@gmail.com>
2024-10-29 22:10:49 +0300

    add

---
+++ Reader/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Reader
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
